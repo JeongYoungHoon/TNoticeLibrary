aa3d94796956117883b20afcb57d6243dae25281
jeyhoon <jeyechoo@gmail.com>
2016-03-03 16:53:15 +0900

    160303 Changed Activity to Fragment

--- app/src/main/java/com/wenoun/library/notice/fragment/Detail.java
+++ app/src/main/java/com/wenoun/library/notice/fragment/Detail.java
@@ -19,7 +19,7 @@ public class Detail extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         try{
-            root=inflater.inflate(R.layout.fragment_empty,container,false);
+            root=inflater.inflate(R.layout.fragment_detail,container,false);
         }catch(Exception e){}
         if(parent.getNotiNo()!=-1){
             ((TextView)root.findViewById(R.id.detail_msg)).setText(DB.Notice.getMsgFromNo(ctx,parent.getNotiNo()));
